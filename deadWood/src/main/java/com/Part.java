423efa988153aa89c5388bce2f825de17f6e3e93
avadanv <avadanv@wwu.edu>
2021-02-10 20:50:10 +0000

    Update Part.java

--- deadWood/src/main/java/com/Part.java
+++ deadWood/src/main/java/com/Part.java
@@ -1,3 +1,12 @@
 class Part{
-    
+    private String partName;
+    private int level;
+    private int[] area;
+    private String line;
+    private int practiceChips;
+    private int shotCounters;
+
+    public Part(String partName, int level, int[] area, String line){
+        
+    }
 }
