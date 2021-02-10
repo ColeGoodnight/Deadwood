9f0783ae6877f1e88cef62bb490ae4932fb35543
avadanv <avadanv@wwu.edu>
2021-02-10 20:26:05 +0000

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,17 +3,45 @@ class Board{
 }
 
 class BoardLocation{
+    private String name;
+    private String[] neighbors;
+    private int[] area;
+    private int[][] takeAreas;
+    private Parts[] setParts;
 
+    public void buildParts(nodeList){
+
+    }
+    public void setBoard(nodeList){
+
+    }
 }
 
 class Trailers{
+    public Trailers(){
 
+    }
 }
 
 class CastingOffice{
+    private int[] upgradeCostsCredit;
+    private int[] upgradeCostsDollars;
 
+    public CastingOffice(nodeList){
+
+    }
 }
 
 class Bank{
-    
+    public Bank(){
+
+    }
+
+    public boolean payPlayer(Player currentPlayer, String currency, int amount){
+        return true;
+    }
+
+    public boolean debtPlayer(Player currentPlayer, String currency, int amount){
+        return true;
+    }
 }
