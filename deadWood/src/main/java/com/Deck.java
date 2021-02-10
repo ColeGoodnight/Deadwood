8fb96ef2cd969657c0428709053362f7f33e9d89
avadanv <avadanv@wwu.edu>
2021-02-10 20:46:34 +0000

    Update Deck.java

--- deadWood/src/main/java/com/Deck.java
+++ deadWood/src/main/java/com/Deck.java
@@ -3,5 +3,18 @@ class Deck{
 }
 
 class Card{
-    
+    private String cardName;
+    private File image;
+    private int budget;
+    private int sceneNum;
+    private String sceneDesc;
+    private Part[] cardParts;
+    private boolean dealt;
+
+    public Card(nodeList){
+
+    }
+    private void buildParts(nodeList){
+        
+    }
 }
