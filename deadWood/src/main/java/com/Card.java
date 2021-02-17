9df657b74f8d552fec77aaecad69aa124d4f1788
goodnic <goodnic@wwu.edu>
2021-02-16 23:08:29 -0800

    Added set/get for card

--- deadWood/src/main/java/com/Card.java
+++ deadWood/src/main/java/com/Card.java
@@ -9,10 +9,44 @@ public class Card {
     private int     sceneNum;
     private String  sceneDesc;
     private Part[]  cardParts;
-
     private String  location;
 
-    public Card(Part[] parts, String cardName, File image, int budget, int sceneNum, String sceneDesc) {
-        
+    public Card(String cardName, File image, int budget, int sceneNum, String sceneDesc, Part[] cardParts, String location) {
+        this.cardName  = cardName;
+        this.image     = image;
+        this.budget    = budget;
+        this.sceneNum  = sceneNum;
+        this.sceneDesc = sceneDesc;
+        this.cardParts = cardParts;
+        this.location  = location;
     }
+
+    public String getCardName() {
+        return this.cardName;
+    }
+
+    public File getImage() {
+        return this.image;
+    }
+
+    public int getBudget() {
+        return this.budget;
+    }
+
+    public int getSceneNum() {
+        return this.sceneNum;
+    }
+
+    public String getSceneDesc() {
+        return this.sceneDesc;
+    }
+
+    public Part[] getCardParts() {
+        return this.cardParts;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
 }
