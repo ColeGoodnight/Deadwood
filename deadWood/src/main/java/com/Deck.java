0ea2e04791c884f06b0cdd187b75c4f3a6d303e1
avadanv <avadanv@wwu.edu>
2021-02-20 04:11:39 +0000

    Update Deck.java

--- deadWood/src/main/java/com/Deck.java
+++ deadWood/src/main/java/com/Deck.java
@@ -5,4 +5,11 @@ import java.io.File;
 class Deck {
     private Card[] cards;
 
+    public Deck(File xmlFile){
+        //put stuff here
+    }
+
+    public void dealRefresh(){
+        //add stuff here
+    }
 }
