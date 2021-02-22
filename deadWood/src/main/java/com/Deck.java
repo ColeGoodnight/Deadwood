dc91370197e5d5357f1e1cc18b3228a6e2bb8d61
avadanv <avadanv@wwu.edu>
2021-02-22 05:13:57 +0000

    Update Deck.java

--- deadWood/src/main/java/com/Deck.java
+++ deadWood/src/main/java/com/Deck.java
@@ -1,12 +1,46 @@
 package com;
 
 import java.io.File;
+import java.util.Random;
 
 class Deck {
-    private Card[] cards;
+    Random randyGuy = new Random();
+
+    private cards = new Card[40];
+
+    private deckCheck = new boolean[40];
+    Arrays.fill(deckCheck, Boolean.FALSE);
 
     public Deck(File xmlFile){
         //put stuff here
+    }
+
+    public void shuffleDeck(){
+        shuffledDeck = new Card[40];
+        int counter = 0;
+        while(!allTrue(deckCheck)){
+            int randomInt = randyGuy.nextInt(40);
+            if(deckCheck[randomInt] == false){  //this card hasn't been accessed yet
+                currentCard = this.cards[randomInt];
+                shuffledDeck[i] = currentCard;
+                i++;
+            }
+            else if(deckCheck[randomInt] == true){ //has been accessed
+                continue;
+            }  
+        }
+    }
+
+    public boolean allTrue(boolean[] deckCheck){
+        boolean check = true;
+        for(int i = 0; i < 40; i++){
+            if(check == deckCheck[i]){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
     }
 
     public void dealRefresh(){
