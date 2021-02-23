f10b2f83c66112b209d10cf1eba7bd54d8319afa
avadanv <avadanv@wwu.edu>
2021-02-23 05:20:50 +0000

    Update DeadwoodView.java

--- deadWood/src/main/java/com/DeadwoodView.java
+++ deadWood/src/main/java/com/DeadwoodView.java
@@ -6,10 +6,14 @@ import com.Deadwood.Admin;
 public class DeadwoodView {
     public static void main(String[] args){
         System.out.println("List of possible commands:");
+        System.out.println("commands");
         System.out.println("Active player?");
         System.out.println("where");
         System.out.println("act");
         System.out.println("who");
+        System.out.println("upgrade");
+        System.out.println("rehearse");
+        System.out.println("end");
         System.out.println("move <location>");
         System.out.println("work <location>");
         System.out.println("<location> in both commands containing it should be replaced with a location of your choice, such as Train Station");
@@ -20,7 +24,7 @@ public class DeadwoodView {
             System.out.print("> ");
             String userInput = terminal.nextLine();
             if (userInput == "end") {
-                activeTerminal = false;
+                //next player
             }
             else if(userInput == "Active player?"){
                 System.out.println("The active plater is " + BoardLocation.getName() + ". They have " + Player.getDollars() + "$, " + Player.getCredits() + " credits and " + Player.getRank() + " fames. They are working " + Player.getCurrentPart());
@@ -29,10 +33,15 @@ public class DeadwoodView {
                 System.out.println(Admin.get);
             }
             else if(userInput == "act"){
-                
+                Admin.getPlayerController().act(Admin.getCurrentPlayer())
             }
-            else if(userInput.contains("work")){
-                
+            else if(userInput.contains("work")){ //cole help pls
+                String part = userInput.substring(4, userInput.length());
+                Admin.getPlayerController().takePart(Admin.getCurrentPlayer(), part)
+            }
+            else if(userInput.contains("upgrade")){ //cole help pls
+                String desiredLevel = userInput.substring(7, userInput.length());
+                Admin.getPlayerController().upgrade(Admin.getCurrentPlayer(), desiredLevel);
             }
             else if(userInput.contains("move ")){
                 String location = userInput.substring(4, userInput.length());
@@ -41,6 +50,20 @@ public class DeadwoodView {
                      .getCurrentPlayer(), 
                      Admin.getBoard()
                           .getBoardLocation(location));
+            }
+            else if(userInput == "commands"){
+                System.out.println("List of possible commands:");
+                System.out.println("commands");
+                System.out.println("Active player?");
+                System.out.println("where");
+                System.out.println("act");
+                System.out.println("who");
+                System.out.println("upgrade");
+                System.out.println("rehearse");
+                System.out.println("end");
+                System.out.println("move <location>");
+                System.out.println("work <location>");
+                System.out.println("<location> in both commands containing it should be replaced with a location of your choice, such as Train Station");
             }
         }
         terminal.close();
